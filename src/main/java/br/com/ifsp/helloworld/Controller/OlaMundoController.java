87ad41391fb047fcc854acf44ba3e214b87825d8
Jose Marcelo Simao <josesimao@guarulhos.sp.gov.br>
2023-05-24 12:19:23 -0300

    Refatorando Olá, Mundo!

---
+++ src/main/java/br/com/ifsp/helloworld/Controller/OlaMundoController.java
@@ -0,0 +1,36 @@
+package br.com.ifsp.helloworld.Controller;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifsp.helloworld.Model.OlaMundo;
+
+@RestController
+@RequestMapping
+public class OlaMundoController {
+
+	List<OlaMundo> olaMundo = new ArrayList<>();
+	
+	public OlaMundoController() {
+		olaMundo.addAll(List.of(new OlaMundo("Olá, mundo!"),
+				new OlaMundo("Hello, world!"),
+				new OlaMundo("Com Boot Spring com RestAPI ..."),
+				new OlaMundo("... nos primeiros passos!")
+				));
+	}
+
+	@GetMapping("olamundo") 
+	String getOlaMundo() {
+		return "Olá, mundo!";
+	}
+	
+	@GetMapping("/restapi")
+	Iterable<OlaMundo> getOlaMundoRestAPI() {
+		return olaMundo;
+	}
+	
+}
---
+++ src/main/java/br/com/ifsp/helloworld/Model/OlaMundo.java
@@ -0,0 +1,16 @@
+package br.com.ifsp.helloworld.Model;
+
+public class OlaMundo {
+	
+	private final String string;
+	
+	public OlaMundo(String string) {
+		// TODO Auto-generated constructor stub
+		this.string = string;
+	}
+
+	public String getString() {
+		return this.string;
+	}
+
+}
