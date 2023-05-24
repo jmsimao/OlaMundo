fa46c5f0a08b1532d79fd8f10e690890f6b4aacf
José Simão <simaojm@gmail.com>
2023-05-24 01:15:10 -0300

    first commit

---
+++ src/main/java/br/com/ifsp/helloworld/HelloworldApplication.java
@@ -0,0 +1,14 @@
+package br.com.ifsp.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HelloworldApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifsp/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
