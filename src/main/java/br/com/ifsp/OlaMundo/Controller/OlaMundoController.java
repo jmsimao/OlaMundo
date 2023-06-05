6840e10317bd6c4e1ea5867ff97876dd6418f6b1
Jose Marcelo Simao <josesimao@guarulhos.sp.gov.br>
2023-06-05 14:35:58 -0300

    Ajustes gerais.

--- src/main/java/br/com/ifsp/OlaMundo/Controller/OlaMundoController.java
+++ src/main/java/br/com/ifsp/OlaMundo/Controller/OlaMundoController.java
@@ -6,49 +6,26 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
 
 import br.com.ifsp.OlaMundo.Model.OlaMundo;
 
 @RestController
-@RequestMapping
-=======
-import br.com.ifsp.OlaMundo.Model.*;
-
-@RestController
 @RequestMapping("/OlaMundo")
->>>>>>> 84cdfbf0cc2d3e95deee989eb3af81f20c88b9d8
 public class OlaMundoController {
 
 	List<OlaMundo> olaMundo = new ArrayList<>();
 	
 	public OlaMundoController() {
-<<<<<<< HEAD
 		olaMundo.addAll(List.of(new OlaMundo("Olá, mundo!"),
 				new OlaMundo("Hello, world!"),
-				new OlaMundo("Com Boot Spring com RestAPI ..."),
+				new OlaMundo("Com Boot Spring e RestAPI ..."),
 				new OlaMundo("... nos primeiros passos!")
 				));
 	}
 
-	@GetMapping("olamundo") 
-	String getOlaMundo() {
-		return "<h1>"
-				+ "<font color=blue>Olá, mundo!</font>"
-				+ "</h1>";
-	}
-	
-	@GetMapping("/restapi")
-	Iterable<OlaMundo> getOlaMundoRestAPI() {
-=======
-		olaMundo.addAll(List.of(new OlaMundo("ola, mundo!"),
-				new OlaMundo("ola, mundo!")));
-	}
-
 	@GetMapping
 	Iterable<OlaMundo> getOlaMundo() {
->>>>>>> 84cdfbf0cc2d3e95deee989eb3af81f20c88b9d8
-		return olaMundo;
+		return this.olaMundo;
 	}
 	
 }
--- src/main/java/br/com/ifsp/OlaMundo/Model/OlaMundo.java
+++ src/main/java/br/com/ifsp/OlaMundo/Model/OlaMundo.java
@@ -2,8 +2,7 @@ package br.com.ifsp.OlaMundo.Model;
 
 public class OlaMundo {
 	
-<<<<<<< HEAD
-	private final String string;
+	private String string;
 	
 	public OlaMundo(String string) {
 		// TODO Auto-generated constructor stub
@@ -14,22 +13,8 @@ public class OlaMundo {
 		return this.string;
 	}
 
-=======
-	private String var1;
-	
-	public OlaMundo(String ola) {
-		// TODO Auto-generated constructor stub
-		this.var1 = ola;
+	public void setString(String string) {
+		this.string = string;
 	}
 
-	public String getVar1() {
-		return var1;
-	}
-
-	public void setVar1(String var1) {
-		this.var1 = var1;
-	}
-
-	
->>>>>>> 84cdfbf0cc2d3e95deee989eb3af81f20c88b9d8
 }
